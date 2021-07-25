2e29998b4a1cc3297c2e830ec5cfc5cd48d4f01d
Gamelegendx <82363939+Gamelegendx@users.noreply.github.com>
2021-07-25 09:54:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 //====================================================================================CONSTANTS REQUIRED ON READY=============================================================================================
-let PORT = process.env.$PORT || 5000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 const { Client, Collection } = require('discord.js');
 const { PREFIX, TOKEN } = require('./config');
 const bot = new Client({ disableMentions: 'everyone' });
